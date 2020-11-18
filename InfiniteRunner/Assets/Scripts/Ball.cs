d9c2d1cb01aaf125d6e2fd5e1117671837f40e63
Praveen Liyanaarachchi <hello@praveen.one>
2020-11-19 08:10:08 +0900

    Platform Generation

---
+++ InfiniteRunner/Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static float DistanceTraveled;
+
+    private float m_left = -2.4f;
+    private float m_Right = 2.4f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        var move = new Vector3(Input.GetAxis("Horizontal") * 0.2f, 0, 0.5f);
+        transform.position += move * 75 * Time.deltaTime;
+        DistanceTraveled = transform.localPosition.z;
+
+        if (transform.position.x < m_left)
+        {
+            transform.position = new Vector3(m_left, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > m_Right)
+        {
+            transform.position = new Vector3(m_Right, transform.position.y, transform.position.z);
+        }
+
+    }
+}
---
+++ InfiniteRunner/Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    [SerializeField] private Transform m_Prefab;
+    [SerializeField] private int m_NumberOfObjects;
+    [SerializeField] private float m_RecycleOffset;
+    [SerializeField] private Vector3 m_StartPosition;
+
+    private Vector3 m_NextPosition;
+    private Queue<Transform> m_ObjectQueue;
+
+    void Start()
+    {
+        m_ObjectQueue = new Queue<Transform>(m_NumberOfObjects);
+        m_NextPosition = m_StartPosition;
+        for (int i = 0; i < m_NumberOfObjects; i++)
+        {
+            Transform platform = (Transform)Instantiate(m_Prefab);
+            platform.transform.parent = transform;
+            platform.localPosition = m_NextPosition;
+            m_NextPosition.z += platform.localScale.z;
+            m_ObjectQueue.Enqueue(platform);
+        }
+    }
+
+    void Update()
+    {
+        if (m_ObjectQueue.Peek().localPosition.z + m_RecycleOffset < Ball.DistanceTraveled)
+        {
+            Transform platform = m_ObjectQueue.Dequeue();
+            platform.localPosition = m_NextPosition;
+            m_NextPosition.z += platform.localScale.z;
+            m_ObjectQueue.Enqueue(platform);
+        }
+    }
+}
